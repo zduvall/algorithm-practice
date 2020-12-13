45305e7097ef71e773526ff6113edda6880b539b
Zachary <duvallzt@gmail.com>
2020-12-13 01:28:41 -0700

    Complete maxDiffContiguous.js

--- maxDiffContiguous.js
+++ maxDiffContiguous.js
@@ -14,6 +14,13 @@ Google Algorithm
  Email me when people comment.
 */
 
+const testArr1 = [2, -1, -2, 1, -4, 2, 8];
+const testArr2 = [10, -9, 8, -8, 9, -10];
+const testArr3 = [1, 2, 6, -7, -9, 7, 8, 6];
+const testArr4 = [4, -9, 1, 5, -9, 7, -8, 6, 5, 3, -9, 1, 0, 7, -2, -6];
+const testArr5 = [1, 3, 1];
+
+
 function maxDiffContiguousArrs(arr) {
     let diffsTable = new Array(arr.length - 1).fill(-Infinity)
 
@@ -31,11 +38,46 @@ function maxDiffContiguousArrs(arr) {
         let diff = Math.abs(leftSum - rightSum);
         diffsTable[i - 1] = diff;
     }
-    return diffsTable;
+    return Math.max(...diffsTable);
 }
 
-console.log(maxDiffContiguousArrs([2, -1, -2, 1, -4, 2, 8]))
-// this one above didn't work because it's assuming all numbers have to be used
+// console.log(maxDiffContiguousArrs(testArr1))
+// console.log(maxDiffContiguousArrs(testArr2))
+// console.log(maxDiffContiguousArrs(testArr3))
+// console.log(maxDiffContiguousArrs(testArr4))
+// // // // // this one above didn't work because it's assuming all numbers have to be used // // // // //
 
 // to do it with a time complexity of O(2n) I need to have two tables
 
+function maxDiffContiguousArrs2(arr) {
+
+    function helper(cb1, cb2) {
+        let l2rTab = new Array(arr.length - 1);
+        let r2lTab = new Array(arr.length - 1);
+
+        let tabLen = r2lTab.length;
+
+        l2rTab[0] = arr[0];
+        r2lTab[tabLen - 1] = arr[arr.length - 1];
+
+        
+        for (let i = 1; i < tabLen; i++) {
+            l2rTab[i] = cb1((arr[i] + l2rTab[i - 1]), arr[i])
+            r2lTab[tabLen - i - 1] = cb2((arr[arr.length - 1 - i] + r2lTab[tabLen - i]), arr[arr.length - 1 - i]);
+        }
+
+        let maxDiff = -Infinity
+
+        for (let i = 0; i < tabLen; i++) {
+            maxDiff = Math.max(Math.abs(l2rTab[i] - r2lTab[i]), maxDiff)
+        }
+        return maxDiff
+    }
+    return Math.max(helper(Math.min, Math.max), helper(Math.max, Math.min))
+}
+
+console.log(maxDiffContiguousArrs2(testArr1)) // 16
+console.log(maxDiffContiguousArrs2(testArr2)) // 18
+console.log(maxDiffContiguousArrs2(testArr3)) // 37
+console.log(maxDiffContiguousArrs2(testArr4)) // 27
+console.log(maxDiffContiguousArrs2(testArr5)) // 3
