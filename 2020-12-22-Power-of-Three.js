bcc95f41db34cf708409488e460e4f8307e1fe9b
Zachary <duvallzt@gmail.com>
2020-12-23 00:50:05 -0800

    refactoring power of 3

--- 2020-12-22-Power-of-Three.js
+++ 2020-12-22-Power-of-Three.js
@@ -5,10 +5,23 @@ An integer n is a power of three, if there exists an integer x such that n == 3x
  */
 
 
+
+// here's an option, a little bit optimized, but more complicated:
+
 const isPowerOfThree = function (n) {
-    while(n >= 3) n = n / 3;
+    if (n > 1 && n % 3 !== 0) return false;
+    while (n >= 3) n = n / 3;
     return n === 1;
 };
+
+// simplest solution below, but not most optimized
+
+// const isPowerOfThree = function (n) {
+//     while (n >= 3) n = n / 3;
+//     return n === 1;
+// };
+
+// // here's another option using log, but not faster
 
 // const isPowerOfThree = function (n) {
 //     return (Math.log(n) / Math.log(3)) % 1 < 0.00000000000001
@@ -25,7 +38,7 @@ const isPowerOfThree = function (n) {
 //         memo[ogN] = true
 //         return memo[ogN]
 //     };
-//     if (n < 3) {
+//     if (n < 3 || n % 3 !== 0) {
 //         memo[ogN] = false
 //         return memo[ogN]
 //     };
