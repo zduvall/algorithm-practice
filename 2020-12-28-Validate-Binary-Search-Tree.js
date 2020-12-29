334d471d4eec8f50ef1458b0ab21c660ad3c2c7e
Zachary <duvallzt@gmail.com>
2020-12-28 23:36:41 -0800

    Update Validate BST

--- 2020-12-28-Validate-Binary-Search-Tree.js
+++ 2020-12-28-Validate-Binary-Search-Tree.js
@@ -54,29 +54,46 @@ const four = new TreeNode(4, three, six);
 const one = new TreeNode(1);
 const five = new TreeNode(5, one, four);
 
-const n7 = new TreeNode(7);
-const n3 = new TreeNode(3);
-const n6 = new TreeNode(6, n3, n7);
+const n8 = new TreeNode(8);
+const n6 = new TreeNode(6);
+const n7 = new TreeNode(7, n6, n8);
 const n4 = new TreeNode(4);
 const n5 = new TreeNode(5, n4, n6);
 
-// here's the solution I came up with, ranged from 26% - 89%, time complexity is linear - O(n)
-// because it just hits every node once
+const node1 = new TreeNode(1)
+const head1 = new TreeNode(1, node1)
 
-function isValidBST(root, prvNd = [-Infinity], isBST = [true]) {
-    if (!root || isBST[0] === false) return;
-    isValidBST(root.left, prvNd, isBST);
-    if (root.val <= prvNd[0]) {
-        isBST[0] = false;
-    } else {
-        prvNd[0] = root.val;
-    }
-    isValidBST(root.right, prvNd, isBST);
-    return isBST[0];
+const node7 = new TreeNode(7);
+const node3 = new TreeNode(3);
+const node6 = new TreeNode(6, node3, node7);
+const node4 = new TreeNode(4);
+const node5 = new TreeNode(5, node4, node6);
+
+
+// here's the solution I came up with, ranged from 26% - 89%, time complexity is linear - O(n)
+// because it just hits every node once. It uses an inorder traversal making sure that each visted
+// node is greater than the previously visited node.
+
+function isValidBST(root, prvNd = { val: -Infinity }, BST = { status: true }) {
+    if (!root || BST.status === false) return;
+
+    isValidBST(root.left, prvNd, BST);
+
+    if (root.val <= prvNd.val) BST.status = false;
+    else prvNd.val = root.val;
+
+    isValidBST(root.right, prvNd, BST);
+
+    return BST.status;
 };
 
-console.log(isValidBST(five));
-console.log(isValidBST(n5));
+console.log(isValidBST(five)); // false
+console.log(isValidBST(n5)); // true
+console.log(isValidBST(head1)); // false
+console.log(isValidBST(node5)); // false
+
+
+
 
 // got this solution from someone else online
 
@@ -87,5 +104,7 @@ function isValidBST2(root, min = null, max = null) {
     return isValidBST2(root.left, min, root) && isValidBST2(root.right, root, max);
 };
 
-console.log(isValidBST2(five));
-console.log(isValidBST2(n5));
+console.log(isValidBST2(five)); // false
+console.log(isValidBST2(n5)); // true
+console.log(isValidBST2(head1)); // false
+console.log(isValidBST2(node5)); // false
