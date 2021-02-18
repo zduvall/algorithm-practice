52726cd74f033033514cd16e250c624bbe8b91dc
Zachary <duvallzt@gmail.com>
2021-02-17 22:56:55 -0700

    Complete kill process

---
+++ 2021-2-17-Kill-Process.py
@@ -0,0 +1,59 @@
+# Given n processes, each process has a unique PID (process id) and its PPID (parent process id).
+
+# Each process only has one parent process, but may have one or more children processes. This is just like a tree structure. Only one process has PPID that is 0, which means this process has no parent process. All the PIDs will be distinct positive integers.
+
+# We use two list of integers to represent a list of processes, where the first list contains PID for each process and the second list contains the corresponding PPID.
+
+# Now given the two lists, and a PID representing a process you want to kill, return a list of PIDs of processes that will be killed in the end. You should assume that when a process is killed, all its children processes will be killed. No order is required for the final answer.
+
+# Example 1:
+# Input: 
+# pid =  [1, 3, 10, 5]
+# ppid = [3, 0, 5, 3]
+# kill = 5
+# Output: [5,10]
+# Explanation: 
+#            3
+#          /   \
+#         1     5
+#              /
+#             10
+# Kill 5 will also kill 10.
+# Note:
+# The given kill id is guaranteed to be one of the given PIDs.
+# n >= 1.
+
+# did this one quickly by looking up a solution
+
+from collections import deque
+
+class Solution:
+    # def killProcess(self, pid: List[int], ppid: List[int], kill: int) -> List[int]:
+    def killProcess(self, pid, ppid, kill):
+        res = []
+        parents = {}
+        queue = deque([kill])
+        
+        for child, parent in zip(pid, ppid):
+            if parent in parents:
+                parents[parent].append(child)
+            else:
+                parents[parent] = [child]
+        
+        while queue:
+            for _ in range(len(queue)):
+                k = queue.popleft()
+                res.append(k)
+                
+                if k in parents:
+                    queue.extend(parents[k])
+                
+        return res
+
+
+pid =  [1, 3, 10, 11, 5]
+ppid = [3, 0, 5, 5, 3]
+kill = 5
+
+test = Solution()
+print(test.killProcess(pid, ppid, kill))
