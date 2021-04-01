8c728e88a6e9cc9c619fc45ff0ce463bc1a27bc1
Zachary <duvallzt@gmail.com>
2021-04-01 12:51:06 -0700

    Complete linked list palindrome in javascript

---
+++ 2021-4-1-Linked-List-Palindrome.js
@@ -0,0 +1,45 @@
+// Palindrome Linked List
+// Given the head of a singly linked list, return true if it is a palindrome.
+
+// Example 1:
+
+// Input: head = [1,2,2,1]
+// Output: true
+// Example 2:
+
+// Input: head = [1,2]
+// Output: false
+
+// Constraints:
+
+// The number of nodes in the list is in the range [1, 105].
+// 0 <= Node.val <= 9
+
+// Follow up: Could you do it in O(n) time and O(1) space?
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {boolean}
+ */
+const isPalindrome = function (head) {
+  const arr = [];
+
+  while (head && head.val >= 0) {
+    arr.push(head.val);
+    head = head.next;
+  }
+
+  const length = arr.length;
+
+  for (let i = 0; i < Math.floor(length / 2); i++) {
+    if (arr[i] !== arr[length - 1 - i]) return false;
+  }
+  return true;
+};
