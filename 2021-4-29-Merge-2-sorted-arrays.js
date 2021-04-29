1ccdfacb3c9d2c798ef0520a77c72ce522e90822
Zachary <duvallzt@gmail.com>
2021-04-29 10:29:11 -0600

    Complete merge 2 sorted arrays, javascript)

---
+++ 2021-4-29-Merge-2-sorted-arrays.js
@@ -0,0 +1,59 @@
+// Merge Sorted Array
+
+// Share
+// Given two sorted integer arrays nums1 and nums2, merge nums2 into nums1 as
+// one sorted array.
+
+// The number of elements initialized in nums1 and nums2 are m and n respectively.
+// You may assume that nums1 has a size equal to m + n such that it has enough
+// space to hold additional elements from nums2.
+
+// Example 1:
+
+// Input: nums1 = [1,2,3,0,0,0], m = 3, nums2 = [2,5,6], n = 3
+// Output: [1,2,2,3,5,6]
+// Example 2:
+
+// Input: nums1 = [1], m = 1, nums2 = [], n = 0
+// Output: [1]
+
+// Constraints:
+
+// nums1.length == m + n
+// nums2.length == n
+// 0 <= m, n <= 200
+// 1 <= m + n <= 200
+// -109 <= nums1[i], nums2[i] <= 109
+
+// function merge(nums1, m, nums2, n) {
+//   newNums1 = nums1.slice(0, m);
+//   const merged = [];
+
+//   while (newNums1.length && nums2.length) {
+//     newNums1[0] < nums2[0]
+//       ? merged.push(newNums1.shift())
+//       : merged.push(nums2.shift());
+//   }
+//   return [...merged, ...newNums1, ...nums2];
+// }
+
+function merge(nums1, m, nums2, n) {
+  let insertPos = m + n - 1;
+  m--;
+  n--;
+  while (n >= 0) {
+    nums1[insertPos--] = nums1[m] > nums2[n] ? nums1[m--] : nums2[n--];
+  }
+}
+
+let nums1 = [1, 2, 3, 0, 0, 0];
+
+merge(nums1, 3, [2, 5, 6], 3);
+
+console.log(nums1);
+
+nums1 = [1];
+
+merge(nums1, 1, [], 0);
+
+console.log(nums1);
