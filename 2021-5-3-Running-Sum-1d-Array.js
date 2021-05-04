b5ee1218b020950f7915a451eb8ace13fee16019
Zachary <duvallzt@gmail.com>
2021-05-03 20:49:00 -0600

    Add function definition

--- 2021-5-3-Running-Sum-1d-Array.js
+++ 2021-5-3-Running-Sum-1d-Array.js
@@ -1,5 +1,5 @@
 // Running Sum of 1d Array
-// Given an array nums. We define a running sum of an array as 
+// Given an array nums. We define a running sum of an array as
 // runningSum[i] = sum(nums[0]…nums[i]).
 
 // Return the running sum of nums.
@@ -23,3 +23,9 @@
 
 // 1 <= nums.length <= 1000
 // -10^6 <= nums[i] <= 10^6
+
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+const runningSum = function (nums) {};
