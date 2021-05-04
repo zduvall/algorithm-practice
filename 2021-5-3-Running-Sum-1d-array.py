ebffd54530909823460abe50fea97e422c42eae6
Zachary <duvallzt@gmail.com>
2021-05-03 21:02:55 -0600

    Complete in pythong

--- 2021-5-3-Running-Sum-1d-array.py
+++ 2021-5-3-Running-Sum-1d-array.py
@@ -30,8 +30,6 @@
 class Solution:
     # def runningSum(self, nums: List[int]) -> List[int]:
     def runningSum(self, nums):
-        if len(nums) == 1:
-            return nums
         for i in range(1, len(nums)):
             nums[i] += nums[i - 1]
         return nums
