705f77e51860eaa9c7a7f8bc61285f9e2282cfd5
Zachary <duvallzt@gmail.com>
2021-05-12 14:31:38 -0600

    Add comments to help with answer

--- 2021-5-12-Range-Sum-Query-2d.py
+++ 2021-5-12-Range-Sum-Query-2d.py
@@ -60,10 +60,10 @@ class NumMatrix:
         for i in range(1, row + 1):
             for j in range(1, col + 1):
                 self.dp[i][j] = (
-                    self.dp[i][j - 1]
+                    matrix[i - 1][j - 1]
+                    + self.dp[i][j - 1]
                     + self.dp[i - 1][j]
                     - self.dp[i - 1][j - 1]
-                    + matrix[i - 1][j - 1]
                 )
 
     def sumRegion(self, row1: int, col1: int, row2: int, col2: int) -> int:
@@ -89,9 +89,9 @@ obj = NumMatrix(
     ]
 )
 
-param_1 = obj.sumRegion(2, 1, 4, 3)
-param_2 = obj.sumRegion(1, 1, 2, 2)
-param_3 = obj.sumRegion(1, 2, 2, 4)
+param_1 = obj.sumRegion(2, 1, 4, 3) # 8
+param_2 = obj.sumRegion(1, 1, 2, 2) # 11
+param_3 = obj.sumRegion(1, 2, 2, 4) # 12
 
 print(param_1)
 print(param_2)
