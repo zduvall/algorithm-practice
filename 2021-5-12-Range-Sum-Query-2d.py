f818b22233bfa57ee488982867c2ef158e8a0895
Zachary <duvallzt@gmail.com>
2021-05-13 10:34:36 -0600

    Add print statement to show how dp works

--- 2021-5-12-Range-Sum-Query-2d.py
+++ 2021-5-12-Range-Sum-Query-2d.py
@@ -66,6 +66,7 @@ class NumMatrix:
                     + self.dp[i - 1][j]
                     - self.dp[i - 1][j - 1]
                 )
+        print(self.dp)
 
     def sumRegion(self, row1: int, col1: int, row2: int, col2: int) -> int:
         return (
