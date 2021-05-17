96b17e15e8f34d972ce0db4dee08d759991de4a4
Zachary <duvallzt@gmail.com>
2021-05-17 15:27:37 -0600

    Add js solution

--- 2021-5-17-Longest-String-Chain.js
+++ 2021-5-17-Longest-String-Chain.js
@@ -1,17 +1,16 @@
 // Longest String Chain
 // Given a list of words, each word consists of English lowercase letters.
 
-// Let's say word1 is a predecessor of word2 if and only if we can add exactly 
+// Let's say word1 is a predecessor of word2 if and only if we can add exactly
 // one letter anywhere in word1 to make it equal to word2. For example, "abc" is
 // a predecessor of "abac".
 
-// A word chain is a sequence of words [word_1, word_2, ..., word_k] with k >= 1, 
+// A word chain is a sequence of words [word_1, word_2, ..., word_k] with k >= 1,
 // where word_1 is a predecessor of word_2, word_2 is a predecessor of word_3,
 // and so on.
 
-// Return the longest possible length of a word chain with words chosen from the 
+// Return the longest possible length of a word chain with words chosen from the
 // given list of words.
-
 
 // Example 1:
 
@@ -28,3 +27,25 @@
 // 1 <= words.length <= 1000
 // 1 <= words[i].length <= 16
 // words[i] only consists of English lowercase letters.
+
+const longestStrChain = function (words) {
+  let memo = {};
+
+  words = words.sort((a, b) => a.length - b.length);
+
+  for (let word of words) {
+    let longest = 0;
+    for (let i = 0; i < word.length; i++) {
+      let pre = word.slice(0, i) + word.slice(i + 1);
+
+      longest = Math.max(longest, (memo[pre] || 0) + 1);
+    }
+
+    memo[word] = longest;
+  }
+
+  return Math.max(...Object.values(memo));
+};
+
+console.log(longestStrChain(['a', 'b', 'ba', 'bca', 'bda', 'bdca'])); // 4
+console.log(longestStrChain(['xbc', 'pcxbcf', 'xb', 'cxbc', 'pcxbc'])); // 5
