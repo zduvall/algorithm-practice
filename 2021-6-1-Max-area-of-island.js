6218e34dd91b4271c1318dc740621154e1834d47
Zachary <duvallzt@gmail.com>
2021-06-01 14:07:19 -0600

    Set up problem and solution

---
+++ 2021-6-1-Max-area-of-island.js
@@ -0,0 +1,71 @@
+// Max Area of Island
+// You are given an m x n binary matrix grid. An island is a group of 1's 
+// (representing land) connected 4-directionally (horizontal or vertical.) You 
+// may assume all four edges of the grid are surrounded by water.
+
+// The area of an island is the number of cells with a value 1 in the island.
+
+// Return the maximum area of an island in grid. If there is no island, return 0.
+
+ 
+
+// Example 1:
+
+
+// Input: grid = [[0,0,1,0,0,0,0,1,0,0,0,0,0],
+//                [0,0,0,0,0,0,0,1,1,1,0,0,0],
+//                [0,1,1,0,1,0,0,0,0,0,0,0,0],
+//                [0,1,0,0,1,1,0,0,1,0,1,0,0],
+//                [0,1,0,0,1,1,0,0,1,1,1,0,0],
+//                [0,0,0,0,0,0,0,0,0,0,1,0,0],
+//                [0,0,0,0,0,0,0,1,1,1,0,0,0],
+//                [0,0,0,0,0,0,0,1,1,0,0,0,0]]
+// Output: 6
+// Explanation: The answer is not 11, because the island must be connected 4-directionally.
+// Example 2:
+
+// Input: grid = [[0,0,0,0,0,0,0,0]]
+// Output: 0
+
+// Constraints:
+
+// m == grid.length
+// n == grid[i].length
+// 1 <= m, n <= 50
+// grid[i][j] is either 0 or 1.
+
+        
+
+const maxAreaOfIsland = function (grid) {
+  let max = 0;
+  for (let i = 0; i < grid.length; i++) {
+    for (let j = 0; j < grid[0].length; j++) {
+      if (grid[i][j] === 1) {
+        let area = islandArea(i, j);
+        max = area > max ? area : max;
+      }
+    }
+  }
+
+  function islandArea(x, y) {
+    let area = 0;
+    if (
+      x < 0 ||
+      y < 0 ||
+      x >= grid.length ||
+      y >= grid[0].length ||
+      grid[x][y] === 0
+    ) {
+      return 0;
+    }
+    grid[x][y] = 0;
+    area =
+      1 +
+      islandArea(x - 1, y) +
+      islandArea(x + 1, y) +
+      islandArea(x, y - 1) +
+      islandArea(x, y + 1);
+    return area;
+  }
+  return max;
+};
