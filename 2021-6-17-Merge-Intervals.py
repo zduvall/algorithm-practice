67d68ca0dfc4da7e5e2dbad054b6530c1a389df1
Zachary <duvallzt@gmail.com>
2021-06-17 10:31:22 -0600

    Add merge intervals

---
+++ 2021-6-17-Merge-Intervals.py
@@ -0,0 +1,19 @@
+def mergeIntervals(intervals):
+    intervals.sort(key=lambda el: el[0])
+
+    ans = [intervals[0]]
+
+    for i in range(1, len(intervals)):
+
+        cur = intervals[i]
+        
+        if cur[0] >= ans[-1][0] and cur[0] <= ans[-1][1]:
+            if cur[1] > ans[-1][1]:
+                ans[-1][1] = cur[1]
+        else:
+            ans.append(cur)
+
+    return ans
+
+
+print(mergeIntervals([[2, 6], [1, 3], [15, 18], [8, 10], [3, 4]]))
