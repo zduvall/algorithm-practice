d4eb03fe622a4a9c4f1e2139a1c67c99b7d613f5
Zachary <duvallzt@gmail.com>
2021-06-16 23:02:14 -0600

    Add necessary import

--- 2021-6-16-Shortest-distance-to-color.py
+++ 2021-6-16-Shortest-distance-to-color.py
@@ -1,15 +1,15 @@
 # Shortest Distance to Target Color
 # You are given an array colors, in which there are three colors: 1, 2 and 3.
 
-# You are also given some queries. Each query consists of two integers i and c, 
-# return the shortest distance between the given index i and the target color c. 
+# You are also given some queries. Each query consists of two integers i and c,
+# return the shortest distance between the given index i and the target color c.
 # If there is no solution return -1.
 
 # Example 1:
 
 # Input: colors = [1,1,2,1,3,2,2,3,3], queries = [[1,3],[2,2],[6,1]]
 # Output: [3,0,3]
-# Explanation: 
+# Explanation:
 # The nearest 3 from index 1 is at index 4 (3 steps away).
 # The nearest 2 from index 2 is at index 2 itself (0 steps away).
 # The nearest 1 from index 6 is at index 3 (3 steps away).
@@ -28,6 +28,11 @@
 # 0 <= queries[i][0] < colors.length
 # 1 <= queries[i][1] <= 3
 
+from typing import List
+
+
 class Solution:
-    def shortestDistanceColor(self, colors: List[int], queries: List[List[int]]) -> List[int]:
+    def shortestDistanceColor(
+        self, colors: List[int], queries: List[List[int]]
+    ) -> List[int]:
         pass
