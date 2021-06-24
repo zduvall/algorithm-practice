fa62e813158504629d7a52fcfcc99fb0b5f891c0
Zachary <duvallzt@gmail.com>
2021-06-23 22:38:55 -0600

    Format instructions and add import

--- 2021-6-23-Leads-to-Destination.py
+++ 2021-6-23-Leads-to-Destination.py
@@ -1,12 +1,14 @@
 # All Paths from Source Lead to Destination
-# Given the edges of a directed graph where edges[i] = [ai, bi] indicates there is an edge between nodes ai and bi, and two nodes source and destination of this graph, determine whether or not all paths starting from source eventually, end at destination, that is:
+# Given the edges of a directed graph where edges[i] = [ai, bi] indicates there 
+# is an edge between nodes ai and bi, and two nodes source and destination of 
+# this graph, determine whether or not all paths starting from source 
+# eventually, end at destination, that is:
 
 # At least one path exists from the source node to the destination node
-# If a path exists from the source node to a node with no outgoing edges, then that node is equal to destination.
+# If a path exists from the source node to a node with no outgoing edges, then 
+# that node is equal to destination.
 # The number of possible paths from source to destination is a finite number.
 # Return true if and only if all roads from source lead to destination.
-
- 
 
 # Example 1:
 
@@ -19,7 +21,8 @@
 
 # Input: n = 4, edges = [[0,1],[0,3],[1,2],[2,1]], source = 0, destination = 3
 # Output: false
-# Explanation: We have two possibilities: to end at node 3, or to loop over node 1 and node 2 indefinitely.
+# Explanation: We have two possibilities: to end at node 3, or to loop over node
+# 1 and node 2 indefinitely.
 # Example 3:
 
 
@@ -30,14 +33,20 @@
 
 # Input: n = 3, edges = [[0,1],[1,1],[1,2]], source = 0, destination = 2
 # Output: false
-# Explanation: All paths from the source node end at the destination node, but there are an infinite number of paths, such as 0-1-2, 0-1-1-2, 0-1-1-1-2, 0-1-1-1-1-2, and so on.
+# Explanation: All paths from the source node end at the destination node, but 
+# there are an infinite number of paths, such as: 
+    # 0-1-2, 
+    # 0-1-1-2, 
+    # 0-1-1-1-2, 
+    # 0-1-1-1-1-2, 
+    # and so on.
 # Example 5:
 
 
 # Input: n = 2, edges = [[0,1],[1,1]], source = 0, destination = 1
 # Output: false
 # Explanation: There is infinite self-loop at destination node.
- 
+
 
 # Constraints:
 
@@ -48,3 +57,9 @@
 # 0 <= source <= n - 1
 # 0 <= destination <= n - 1
 # The given graph may have self-loops and parallel edges.
+
+from typing import List
+
+class Solution:
+    def leadsToDestination(self, n: int, edges: List[List[int]], source: int, destination: int) -> bool:
+        
