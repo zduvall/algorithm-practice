fdf86202121524e957ea72d60c89e4d9ac4bbc1c
Zachary <duvallzt@gmail.com>
2021-06-24 23:17:04 -0600

    Add a solution

--- 2021-6-24-Out-of-boundary-paths.py
+++ 2021-6-24-Out-of-boundary-paths.py
@@ -23,5 +23,22 @@
 # 0 <= startRow < m
 # 0 <= startColumn < n
 
+
 class Solution:
-    def findPaths(self, m: int, n: int, maxMove: int, startRow: int, startColumn: int) -> int:
+    def findPaths(
+        self, m: int, n: int, maxMove: int, startRow: int, startColumn: int
+    ) -> int:
+        def solve(i, j, maxMove):
+            if maxMove < 0:
+                return 0
+            if i < 0 or i >= m or j < 0 or j >= n:
+                return 1
+
+            a = solve(i - 1, j, maxMove - 1)
+            b = solve(i + 1, j, maxMove - 1)
+            c = solve(i, j - 1, maxMove - 1)
+            d = solve(i, j + 1, maxMove - 1)
+
+            return a + b + c + d
+
+        return solve(startRow, startColumn, maxMove) % 1000000007
